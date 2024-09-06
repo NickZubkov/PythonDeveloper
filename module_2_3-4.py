5462c41aa8537d24a592d2e6249388b9f43fb14f
NikolayZubkov <zubk0vna31@gmail.com>
2024-09-07 04:18:02 +0600

    home work module 2_3-4

---
+++ module_2_3-4.py
@@ -0,0 +1,13 @@
+﻿my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+idx = 0
+while idx < len(my_list):
+    if my_list[idx] > 0:
+        print(my_list[idx])
+        idx += 1
+        continue
+    elif my_list[idx] == 0:
+        idx += 1
+        continue
+    else:
+        break
+
