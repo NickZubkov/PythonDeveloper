2f0cb45283a9b1fc8677cef4abcb5c640cfe1607
NikolayZubkov <zubk0vna31@gmail.com>
2024-09-07 06:54:36 +0600

    home work module 2_hard

---
+++ module_2_hard.py
@@ -0,0 +1,16 @@
+﻿import random
+def get_password():
+    random_number = random.randint(3, 20)
+    print(random_number)
+    password = []
+    for i in range(1, random_number):
+        for j in range(i, random_number):
+            if i == j:
+                continue
+            if random_number % (i + j) == 0:
+                password.append([i, j])
+    return password
+print(get_password())
+print(get_password())
+print(get_password())
+print(get_password())
