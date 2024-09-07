2c23d9dd2ffeaecf453ee15d68afd97adce73f10
NikolayZubkov <zubk0vna31@gmail.com>
2024-09-07 07:26:58 +0600

    homework module 3_1

---
+++ module_3/module_3_1.py
@@ -0,0 +1,25 @@
+﻿calls = 0
+
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()
+    return len(string), string.upper(), string.lower()
+
+def is_contains(string, strings_list):
+    count_calls()
+    contains = False
+    for i in strings_list:
+        if str(string.lower()) in str(i.lower()):
+            contains = True
+            break
+    return contains
+
+print(string_info("hello"))
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(calls)
