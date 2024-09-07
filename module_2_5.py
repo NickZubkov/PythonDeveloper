ef10d348e7d2e1cf103defe8b2e2af92054057d7
NikolayZubkov <zubk0vna31@gmail.com>
2024-09-07 06:54:27 +0600

    home work module 2_5

---
+++ module_2_5.py
@@ -0,0 +1,8 @@
+﻿def get_matrix(n ,m, value):
+    matrix = []
+    for i in range(0, n):
+        matrix.append([])
+        for j in range(0, m):
+            matrix[i].append(value)
+    return matrix
+print(get_matrix(3, 4, 5))
