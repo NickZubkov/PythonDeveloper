868cec7a494d50be583fe326f31e16b1b465d46e
NikolayZubkov <zubk0vna31@gmail.com>
2024-09-08 23:51:45 +0600

    homework module 3_3

---
+++ module_3/module_3_3.py
@@ -0,0 +1,16 @@
+﻿def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
+
+values_list = [54.32, 'Строка', False]
+values_list_2 = ['Hello', -5]
+values_dict = {"a" : True, "b" : False, "c" : None}
+print_params()
+print_params(True, 3, "string")
+print_params(values_list)
+print_params(*values_list)
+print_params(values_list_2)
+print_params(*values_list_2)
+print_params(*values_list_2, 43)
+print_params(values_dict, 5)
+print_params(*values_dict)
+print_params(**values_dict)
