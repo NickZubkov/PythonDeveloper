12353835840e0efc25f3eef1ec907200dd5890ea
NikolayZubkov <zubk0vna31@gmail.com>
2024-09-09 00:43:44 +0600

    homework module 1_5 rework

--- module_1/module_1_5.py
+++ module_1/module_1_5.py
@@ -2,6 +2,10 @@
 print(immutable_var)
 immutable_var[-1][-1] = False
 print(immutable_var)
+tmp_list = [*immutable_var]
+tmp_list[0] = "I can change it"
+immutable_var = tuple(tmp_list)
+print(f"Changed tuple value {immutable_var}")
 mutable_list = [1, True, "Hello", [1, True, "Hello"]]
 print(mutable_list)
 mutable_list[0] = False
