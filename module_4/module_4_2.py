a0bf8db50fa8ee7a7655cb4a562c62ec79df6040
NikolayZubkov <zubk0vna31@gmail.com>
2024-10-09 02:48:13 +0600

    homework module_4_2

---
+++ module_4/module_4_2.py
@@ -0,0 +1,14 @@
+﻿def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+    inner_function()
+
+test_function()
+
+#inner_function()
+
+#Traceback (most recent call last):
+#  File "C:\Users\Николай\PycharmProjects\HomeWorks\module_4\module_4_2.py", line 8, in <module>
+#    inner_function()
+#    ^^^^^^^^^^^^^^
+#NameError: name 'inner_function' is not defined. Did you mean: 'test_function'?
