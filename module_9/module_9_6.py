30114ce7fe03f4662628b5526b53639410013810
NikolayZubkov <zubk0vna31@gmail.com>
2024-11-06 15:41:52 +0600

    homework module_9_6

---
+++ module_9/module_9_6.py
@@ -0,0 +1,13 @@
+﻿
+
+def all_variants(text):
+    length = len(text)
+    for x in range(1, length + 1):
+        for y in range(length - x + 1):
+            yield text[y:y + x]
+
+a = all_variants("abc")
+for i in a:
+    print(i)
+
+
