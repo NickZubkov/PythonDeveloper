8761abe5db7f84749df3b6c453aed2c969b23f80
NikolayZubkov <zubk0vna31@gmail.com>
2025-01-07 04:41:08 +0600

    homework module_12_4 correction

--- module_12/tests_12_1.py
+++ module_12/tests_12_1.py
@@ -2,41 +2,34 @@
 # -*- coding: UTF-8 -*-
 import logging
 import unittest
-from module_12.runner_and_tournament import Runner
+from module_12.rt_with_exceptions import Runner
 
 class RunnerTest(unittest.TestCase):
     is_frozen = False
 
     @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены'")
     def test_walk(self):
-        r = Runner("Walk test", -5)
         try:
-            if r.speed < 0:
-                raise Exception("Скорость не может быть отрицательной")
+            r = Runner("Walk test", -5)
             for x in range(10):
                 r.walk()
             logging.info("test_walk выполнен успешно")
-        except Exception as e:
-            logging.warning(e)
-        finally:
             self.assertEqual(r.distance, 50)
-
-
+        except ValueError as e:
+            logging.warning(e)
+            self.fail(e)
 
     @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены'")
     def test_run(self):
-        r = Runner(5)
         try:
-            if not isinstance(r.name, str):
-                raise Exception("Неверный тип данных для объекта Runner")
+            r = Runner(5)
             for x in range(10):
                 r.run()
             logging.info("test_run выполнен успешно")
-        except Exception as e:
-            logging.warning(e)
-        finally:
             self.assertEqual(r.distance, 100)
-
+        except TypeError as e:
+            logging.warning(e)
+            self.fail(e)
 
     @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены'")
     def test_challenge(self):
