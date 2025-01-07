3eee5919c4c8d00a9aabf92975f26fad75ff6b70
NikolayZubkov <zubk0vna31@gmail.com>
2025-01-08 01:29:03 +0600

    homework module_13_1

---
+++ module_13/module_13_1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import asyncio
+
+async def start_strongman(name, power):
+    print(f"Силач {name} начал соревнования.")
+    for x in range(1, 6):
+        await asyncio.sleep(1/power)
+        print(f"Силач {name} поднял {x} шар")
+    print(f"Силач {name} закончил соревнования.")
+
+async def start_tournament():
+    task1 = asyncio.create_task(start_strongman("Pasha", 3))
+    task2 = asyncio.create_task(start_strongman("Denis", 4))
+    task3 = asyncio.create_task(start_strongman("Apollon", 5))
+    await task1
+    await task2
+    await task3
+
+asyncio.run(start_tournament())
